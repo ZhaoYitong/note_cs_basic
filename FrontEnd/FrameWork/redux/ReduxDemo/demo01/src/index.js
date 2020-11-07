d515d921dcf46c63d950e48e55867b9daedf8b1b
Kerouac <yitong.zhao@cloudchef.io>
2020-11-08 02:32:55 +0800

    delete

--- FrontEnd/FrameWork/redux/ReduxDemo/demo01/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoList from './TodoList'
-
-ReactDOM.render(<TodoList></TodoList>,
-  document.getElementById('root')
-);
--- FrontEnd/FrameWork/redux/index.md
+++
@@ -1,1 +0,0 @@
-![redux_data_flow](img\redux_data_flow.PNG)
