19e023d5e21edaf76302f5766067ab7b93252be5
Kerouac <yitong.zhao@cloudchef.io>
2021-02-08 02:01:08 +0800

    add

---
+++ FrontEnd/FrameWork/redux/ReduxDemo/demo01/src/TodoList.js
@@ -0,0 +1,17 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+import 'antd/es/'
+
+const TodoList = memo(function TodoList(props) {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+})
+
+TodoList.propTypes = {
+
+}
+
+export default TodoList
