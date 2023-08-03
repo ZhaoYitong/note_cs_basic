7c8a17957ffd2142c446e64c29d96fe59531d14a
Sicilian <wode2016zyt@outlook.com>
2023-08-03 06:58:51 +0000

    promise todo

--- FrontEnd/JavaScript/buitInJs/promise.ts
+++ FrontEnd/JavaScript/buitInJs/promise.ts
@@ -1,72 +1,7 @@
-const test = Promise.resolve(() => {
-  console.log("ddd");
-});
-const b = test.then((res) => {
-  res();
-});
-const dc = test
-  .then()
-  .then()
-  .then()
-  .then((f) => f());
+// promise 源码实现参考库
+// https://github.com/stefanpenner/es6-promise
+// https://github.com/then/promise
 
-class Interceptor {
-  actions: Promise<any>;
-  constructor() {
-    this.actions = Promise.resolve();
-  }
+// 一种手写js的简易promise实现 https://github.com/YvetteLau/Blog/issues/2
 
-  use(func: any) {
-    this.actions = this.actions.then(func);
-  }
-
-  run() {
-    return this.actions;
-  }
-}
-
-const interceptor = new Interceptor();
-
-// 添加第一个拦截器函数
-interceptor.use(() => {
-  console.log("First function");
-  return "First function";
-});
-console.log(interceptor);
-
-interceptor.run().then((finalResult) => {
-  console.log("Final result - First function:", finalResult);
-});
-
-// 添加第二个拦截器函数
-interceptor.use((prevResult: any) => {
-  console.log("Second function, previous result:", prevResult);
-  return "Second function";
-});
-
-// 添加第三个拦截器函数
-interceptor.use((prevResult: any) => {
-  console.log("Third function, previous result:", prevResult);
-  return "Third function";
-});
-
-// interceptor.actions
-
-// 执行拦截器链
-interceptor.run().then((finalResult) => {
-  console.log("Final result:", finalResult);
-});
-
-/////////////////////////////////////////////////////////////////
-
-const testFn = () => {
-  if (1) {
-    console.log(1)
-    setTimeout(() => {
-      console.log(2)
-    }, 2);
-    console.log(3)
-  }
-}
-
-testFn()
+// TODO: 手动实现一个promise，支持各种异常机制判定，引入 all, race, allSettled 等常规方法
