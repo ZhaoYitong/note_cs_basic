f090902fb814b6fa78150a804cd611af89d14d6e
ZhaoYitong <wode2016zyt@outlook.com>
2024-04-26 05:56:07 +0000

    cancelable promise

---
+++ ComputerScience/DataStrutureAndAlgorithm/interview/04.ts
@@ -0,0 +1,20 @@
+// 可以取消的promise
+
+export default function makeCancellablePromise<T>(promise: Promise<T>) {
+    let isCancelled = false;
+  
+    const wrappedPromise: typeof promise = new Promise((resolve, reject) => {
+      promise
+        .then((value) => !isCancelled && resolve(value))
+        .catch((error) => !isCancelled && reject(error));
+    });
+  
+    return {
+      promise: wrappedPromise,
+      cancel() {
+        isCancelled = true;
+      },
+    };
+  }
+
+  
